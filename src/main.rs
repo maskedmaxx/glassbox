4acdfcb8e1c7b4da42ba2251d0904e271c514832
maskedmaxx <dmax0181@gmail.com>
2026-07-03 22:41:30 -0400

    define audit command args

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod audit;
+mod cli;
+mod docker;
+mod report;
+mod rules;
+
+use anyhow::Result;
+use cli::{Cli, Command};
+
+fn main() -> Result<()> {
+    let cli = Cli::parse_args();
+
+    match cli.command {
+        Command::Audit(args) => audit::run(args),
+    }
+}
